5b67e1a1f18b9cebff9c0f5488097556fd51d6f8
WilShi <47671779+WilShi@users.noreply.github.com>
2022-01-14 18:04:57 +0800

    Add files via upload

--- stock_test.py
+++ stock_test.py
@@ -6,4 +6,7 @@ import pandas as pd
 # 接口教程：https://cloud.tencent.com/developer/article/1866258
 
 fund_em_open_fund_daily_df = ak.fund_em_open_fund_daily()
-fund_em_open_fund_daily_df.to_csv("基金数据.csv", index=0, encoding='utf-8_sig')
+fund_em_open_fund_daily_df.to_csv("开放式基金数据.csv", index=0, encoding='utf-8_sig')
+
+fund_em_fund_name_df = ak.fund_em_fund_name()
+fund_em_fund_name_df.to_csv("混合型基金数据.csv", index=0, encoding='utf-8_sig')
